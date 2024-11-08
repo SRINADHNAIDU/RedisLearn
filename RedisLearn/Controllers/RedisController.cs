4338d4c8b1f5b6fc81ba848eedb1ea0c12f561a0
SRINADHNAIDU <srinadh949374@gmail.com>
2024-11-08 15:57:39 +0530

    Added connection to redis and tested with basic examples

---
+++ RedisLearn/Controllers/RedisController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Redis.OM;
+using Redis.OM.Searching;
+using RedisLearn.Models;
+
+namespace RedisLearn.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class RedisController(RedisConnectionProvider provider) : ControllerBase
+{
+    private readonly RedisCollection<Person> _people = (RedisCollection<Person>)provider.RedisCollection<Person>();
+
+    [HttpPost("createPerson")]
+    public async Task<Person> AddPerson([FromBody] Person person)
+    {
+        await _people.InsertAsync(person);
+        return person;
+    }
+    
+    [HttpGet("GetAllPeople")]
+    public async Task<IActionResult> Index()
+    {
+        try
+        {
+            return Ok(await _people.ToListAsync());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ RedisLearn/Services/IndexCreateService.cs
@@ -0,0 +1,17 @@
+﻿using Redis.OM;
+using RedisLearn.Models;
+
+namespace RedisLearn.Services;
+
+public class IndexCreationService(RedisConnectionProvider provider) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await provider.Connection.CreateIndexAsync(typeof(Person));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
